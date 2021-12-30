b1fb3ba5dd79fb65317cfa94b2f068e4cf47f410
Julia <juliatangwc@gmail.com>
2021-12-30 01:21:06 +0000

    Created initial templates and css file

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, session, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def function():
-    return 
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
